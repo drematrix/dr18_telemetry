5dcd688b07f65043ec2fce4fd3b2e02bd8a5bda0
NaikSingh <adhirajsingh1206@gmail.com>
2019-01-04 21:54:49 +0530

    Addition of GPS.

--- arduino_codes/linearPots/linearPots.ino
+++ arduino_codes/linearPots/linearPots.ino
@@ -1,21 +1,46 @@
-const int fLeft = 4;
-const int fRight = 5;
-const int rLeft = 6;
-const int rRight = 7;
+const int fLeft = 4 ;
+const int fRight = 5 ; 
+const int rLeft = 6 ;
+const int rRight = 7 ;
+const int GPS = 3 ; 
 
 void setup() {
-  Serial.begin(9600); // Initialize Serial to log output  
+  Serial.begin(9600) ; // Initialize Serial to log output  
 }
 
 void loop() {
+
 // Following 4 inputs are for the linear potentials 
 
-  Serial.print(analogRead(fLeft)); 
-  Serial.print(" ");
-  Serial.print(analogRead(fRight)); 
-  Serial.print(" "); 
-  Serial.print(analogRead(rLeft)); 
-  Serial.print(" "); 
-  Serial.println(analogRead(rRight)); 
+  Serial.print(analogRead(fLeft)) ; 
+  Serial.print(" ") ;
+  Serial.print(analogRead(fRight)) ; 
+  Serial.print(" ") ; 
+  Serial.print(analogRead(rLeft)) ; 
+  Serial.print(" ") ; 
+  Serial.print(analogRead(rRight)) ; 
+  Serial.print(" ") ; 
+  
+// Following input is for the Gear Position Sensor 
+
+  int GPS_val = analogRead(GPS) ; 
+
+    if(GPS_val > 111 && GPS_val < 121) { 
+      Serial.println("1") ; 
+    }else if(GPS_val > 189 && GPS_val < 199) { 
+      Serial.println("N") ; 
+    }else if(GPS_val > 269 && GPS_val < 279) { 
+      Serial.println("2") ; 
+    }else if(GPS_val > 427 && GPS_val < 437) { 
+      Serial.println("3") ; 
+    }else if(GPS_val > 586 && GPS_val < 596) { 
+      Serial.println("4") ; 
+    }else if(GPS_val > 745 && GPS_val < 754) { 
+      Serial.println("5") ; 
+    }else if(GPS_val > 902 && GPS_val < 912) { 
+      Serial.println("6") ; 
+    }else { 
+      Serial.println("E") ; 
+    }
   
 }
