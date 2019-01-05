a340c03124aa710832effdef321cf9a4bc7b9586
NaikSingh <adhirajsingh1206@gmail.com>
2019-01-06 01:50:24 +0530

    Addition of Lap Timimg Button.

--- arduino_codes/linearPots/linearPots.ino
+++ arduino_codes/linearPots/linearPots.ino
@@ -1,4 +1,4 @@
-// Components: 4 Linear Potentials , 1 Gear Position Sensor   
+// Components: 4 Linear Potentials , 1 Gear Position Sensor , 1 Lap Timing Button 
 
 const int fLeft = 4 ;
 const int fRight = 5 ; 
@@ -7,7 +7,8 @@ const int rRight = 7 ;
 const int GPS = 3 ; 
 
 void setup() {
-  Serial.begin(9600) ; // Initialize Serial to log output  
+  Serial.begin(9600) ; // Initialize Serial to log output
+  pinMode(A0,INPUT)  
 }
 
 void loop() {
@@ -28,21 +29,25 @@ void loop() {
   int GPS_val = analogRead(GPS) ; 
 
     if(GPS_val > 111 && GPS_val < 121) { 
-      Serial.println("1") ; 
+      Serial.print("1") ; 
     }else if(GPS_val > 189 && GPS_val < 199) { 
-      Serial.println("N") ; 
+      Serial.print("N") ; 
     }else if(GPS_val > 269 && GPS_val < 279) { 
-      Serial.println("2") ; 
+      Serial.print("2") ; 
     }else if(GPS_val > 427 && GPS_val < 437) { 
-      Serial.println("3") ; 
+      Serial.print("3") ; 
     }else if(GPS_val > 586 && GPS_val < 596) { 
-      Serial.println("4") ; 
+      Serial.print("4") ; 
     }else if(GPS_val > 745 && GPS_val < 754) { 
-      Serial.println("5") ; 
+      Serial.print("5") ; 
     }else if(GPS_val > 902 && GPS_val < 912) { 
-      Serial.println("6") ; 
+      Serial.print("6") ; 
     }else { 
-      Serial.println("E") ; 
+      Serial.print("E") ; 
     }
   
+// Following input is for Lap Timing Button 
+
+  Serial.println(digitalRead(A0)) ; 
+
 }
